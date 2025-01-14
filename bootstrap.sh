cef973eb649eed733429ad22385313e05d7f4eee
Fabian Hirschmann <fabian@hirschmann.email>
2025-01-14 17:47:57 +0100

    reindent bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,29 +6,33 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
-
 NC='\033[0m' # No Color
 MSG="${RED}Please edit vars.sh and execute \`source vars.sh\` before running this script again.${NC}"
-[ -z $PROJECT_ID ]  && echo -e "\$PROJECT_ID is not set. ${MSG}" && exit 1
-[ -z $GCP_USERNAME ]  && echo -e "\$GCP_USERNAME is not set. ${MSG}" && exit 1
-[ -z $REGION ]  && echo -e "\$GCP_USERNAME is not set. ${MSG}" && exit 1
+
+[ -z $PROJECT_ID ] && echo -e "\$PROJECT_ID is not set. ${MSG}" && exit 1
+[ -z $GCP_USERNAME ] && echo -e "\$GCP_USERNAME is not set. ${MSG}" && exit 1
+[ -z $REGION ] && echo -e "\$GCP_USERNAME is not set. ${MSG}" && exit 1
 
 set -x # print out commands that are being executed
 
 # Enable necessary APIs
-gcloud services enable storage-component.googleapis.com notebooks.googleapis.com serviceusage.googleapis.com cloudresourcemanager.googleapis.com pubsub.googleapis.com compute.googleapis.com metastore.googleapis.com datacatalog.googleapis.com analyticshub.googleapis.com bigquery.googleapis.com dataplex.googleapis.com datalineage.googleapis.com dataform.googleapis.com dataproc.googleapis.com bigqueryconnection.googleapis.com aiplatform.googleapis.com
+gcloud services enable storage-component.googleapis.com notebooks.googleapis.com \
+    serviceusage.googleapis.com cloudresourcemanager.googleapis.com pubsub.googleapis.com \
+    compute.googleapis.com metastore.googleapis.com datacatalog.googleapis.com analyticshub.googleapis.com \
+    bigquery.googleapis.com dataplex.googleapis.com datalineage.googleapis.com dataform.googleapis.com \
+    dataproc.googleapis.com bigqueryconnection.googleapis.com aiplatform.googleapis.com
 
 # Install Git and Git LFS
 if ! $(git lfs >/dev/null); then
-  sudo apt-get update
-  sudo apt-get install git
-  sudo apt-get install git-lfs
+    sudo apt-get update
+    sudo apt-get install git
+    sudo apt-get install git-lfs
 fi
 
 # Retrieve data files
 if [ ! -d "bootkon-data" ]; then
-  git clone https://github.com/fhirschmann/bootkon-data.git
-  cd bootkon-data && git lfs pull
+    git clone https://github.com/fhirschmann/bootkon-data.git
+    cd bootkon-data && git lfs pull
 fi
 
 gcloud config set project $PROJECT_ID
@@ -51,9 +55,9 @@ declare -a user_roles=(
 
 # Assign roles to user
 for role in "${user_roles[@]}"; do
-  echo "Assigning role $role to $USER_EMAIL in project $PROJECT_ID..."
-  gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member="user:$GCP_USERNAME" --role="$role" >>/dev/null
+    echo "Assigning role $role to $USER_EMAIL in project $PROJECT_ID..."
+    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+        --member="user:$GCP_USERNAME" --role="$role" >>/dev/null
 done
 
 # Retrieve the project number for the default compute service account
@@ -61,8 +65,8 @@ PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectN
 
 # Check if we successfully retrieved the project number
 if [ -z "$PROJECT_NUMBER" ]; then
-  echo "Failed to get the project number for project ID $PROJECT_ID"
-  exit 1
+    echo "Failed to get the project number for project ID $PROJECT_ID"
+    exit 1
 fi
 # Define service account email using the project number
 COMPUTE_SERVICE_ACCOUNT="$PROJECT_NUMBER-compute@developer.gserviceaccount.com"
@@ -82,31 +86,31 @@ declare -a service_account_roles=(
 
 # Assign roles to the compute service account
 for role in "${service_account_roles[@]}"; do
-  echo "Assigning role $role to $COMPUTE_SERVICE_ACCOUNT in project $PROJECT_ID..."
-  gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member="serviceAccount:$COMPUTE_SERVICE_ACCOUNT" --role="$role" >>/dev/null
+    echo "Assigning role $role to $COMPUTE_SERVICE_ACCOUNT in project $PROJECT_ID..."
+    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+        --member="serviceAccount:$COMPUTE_SERVICE_ACCOUNT" --role="$role" >>/dev/null
 done
 
 # Create default VPC network and enable private access
 if $(gcloud compute networks describe default 2>>/dev/null 1>>/dev/null); then
-  echo -e "${GREEN}VPC network named 'default' already exists. Not recreating${NC}"
+    echo -e "${GREEN}VPC network named 'default' already exists. Not recreating${NC}"
 else
-  SUBNET="default"
-  gcloud compute networks create $SUBNET --project=$PROJECT_ID --subnet-mode=auto --bgp-routing-mode="regional"
-  gcloud compute networks subnets update $SUBNET --region=$REGION --enable-private-ip-google-access
-  gcloud compute firewall-rules create "default-allow-all-internal" \
-    --network="default" \
-    --project=$PROJECT_ID \
-    --direction=INGRESS \
-    --priority=65534 \
-    --source-ranges="10.128.0.0/9" \
-    --allow=tcp:0-65535,udp:0-65535,icmp
+    SUBNET="default"
+    gcloud compute networks create $SUBNET --project=$PROJECT_ID --subnet-mode=auto --bgp-routing-mode="regional"
+    gcloud compute networks subnets update $SUBNET --region=$REGION --enable-private-ip-google-access
+    gcloud compute firewall-rules create "default-allow-all-internal" \
+        --network="default" \
+        --project=$PROJECT_ID \
+        --direction=INGRESS \
+        --priority=65534 \
+        --source-ranges="10.128.0.0/9" \
+        --allow=tcp:0-65535,udp:0-65535,icmp
 fi
 
 # Create a Google Cloud Storage bucket
 BUCKET_NAME="${PROJECT_ID}-bucket"
 if ! $(gsutil ls -b gs://${BUCKET_NAME} 2>>/dev/null 1>>/dev/null); then
-  gsutil mb -l $REGION gs://${BUCKET_NAME}
+    gsutil mb -l $REGION gs://${BUCKET_NAME}
 fi
 
 # Copy files to GCS
