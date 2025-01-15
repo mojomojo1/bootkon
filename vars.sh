f9092335132d2601716be4f4734399aa9e62dcae
Fabian Hirschmann <fabian@hirschmann.email>
2025-01-15 07:57:22 +0100

    don't start tutorial in debug mode

--- vars.sh
+++ vars.sh
@@ -2,5 +2,5 @@ export PROJECT_ID="" # example: bootkon-data-3472
 export GCP_USERNAME="" # example: devstar3110@gcplab.me
 export REGION="us-central1" # do not change
 
-alias r='cloudshell launch-tutorial -d TUTORIAL.md'
+alias r='cloudshell launch-tutorial TUTORIAL.md'
 gcloud config set project $PROJECT_ID
