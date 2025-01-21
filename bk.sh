903c3d5c60e444318d704c3c21f000c540c03ca9
Fabian Hirschmann <fabian@hirschmann.email>
2025-01-21 21:00:03 +0000

    fix permissions

---
+++ bk.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Bootkon init script. Executing it will clone bootkon,
+# open it in Cloud Shell, and launch the tutorial.
+#
+# Author: Fabian Hirschmann
+#
+# You can run this command directly from GitHub:
+#   curl s
+# or locally:
+#   BK_REPO=fhirschmann/bootkon . bk.sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+err() {
+  echo -e "${RED}Error: $1${NC}" >&2
+}
+
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+    err 'Script is not sourced. Please source it.'
+    err 'Example: BK_REPO=fhirschmann/bootkon . bk.sh'
+    exit 1
+fi
+
+if [ -z $BK_REPO ]; then
+    err 'Variable BK_REPO is not set. Please set it to a GitHub username and repository'
+    err 'Example: BK_REPO=fhirschmann/bootkon . bk.sh'
+    return 1
+fi
+
+BK_GITHUB_USERNAME=$(echo $BK_REPO | cut -d/ -f1) # first part of fhirschmann/bootkon
+BK_GITHUB_REPOSITORY=$(echo $BK_REPO | cut -d/ -f2) # second part of fhirschmann/bootkon
+BK_REPO_URL="https://github.com/${BK_REPO}.git"
+BK_TUTORIAL="${BK_TUTORIAL:-.TUTORIAL.md}" # defaults to .TUTORIAL.md; can be overwritten
+
+cd ~/
+
+if [ -d $BK_GITHUB_REPOSITORY ]; then
+    echo -e "${GREEN}Not cloning $BK_REPO_URL because folder $BK_GITHUB_REPOSITORY already exists.${NC}"
+else
+    echo -e "Cloning $BK_REPO_URL into $BK_GITHUB_REPOSITORY..."
+    git clone https://github.com/${BK_REPO}.git
+fi
+
+cd bootkon
+cloudshell open-workspace .
+
+echo -e "${MAGENTA}Loading tutorial from ${BK_TUTORIAL}${NC}"
+
+teachme $BK_TUTORIAL
+# Run it twice due to bug in pantheon
+teachme $BK_TUTORIAL
+
+NEW_PATH=~/${BK_GITHUB_REPOSITORY}/docs/scripts
+
+# Check if the new path is already in the PATH
+if [[ ":$PATH:" != *":$NEW_PATH:"* ]]; then
+echo -e "${MAGENTA}Adding $NEW_PATH to your PATH${NC}"
+    export PATH=${NEW_PATH}:$PATH
+else
+    echo -e "${GREEN}Your path already contains $NEW_PATH. Not adding it again.${NC}"
+fi
