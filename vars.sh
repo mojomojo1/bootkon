aab46cbd0d0165edd0bb4a68d46d88cdcdc27826
Cary Edwards <cary.edwards@gmx.de>
2025-02-21 11:09:23 +0000

    delete GCP_USERNAME_SHORT

--- vars.sh
+++ vars.sh
@@ -1,5 +1,4 @@
 export MY_NAME="" # example: Ada
 export PROJECT_ID="" # example: bootkon-data-3472
 export GCP_USERNAME="" # example: devstar3110@gcplab.me
-export GCP_USERNAME_SHORT="" # example: devstar3110
 export REGION="us-central1" # do not change this value
